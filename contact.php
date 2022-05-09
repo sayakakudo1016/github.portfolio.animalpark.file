af9f071cec6210ed65baab8e9caa3abb7f94830e
{saya1313} <{sayaka.kudo.adobe@gmail.com}>
2022-05-09 21:09:51 +0900

    commit

--- contact.php
+++ contact.php
@@ -1,26 +1,55 @@
 <?php
   session_start();
-  $mode = "input";
-  if( isset($_POST["back"] ) && $_POST["back"] ){
+  $mode = 'input';
+  $errmessage = array();
+  if( isset($_POST['back']) && $_POST['back'] ){
     // 何もしない
-  } else if( isset($_POST["confirm"] ) && $_POST["confirm"] ){
-    $_SESSION["fullname"] = $_POST["fullname"];
-    $_SESSION["email"] = $_POST["email"];
-    $_SESSION["message"] = $_POST["message"];
-    $mode = "confirm";
-  } else if( isset($_POST["send"] ) && $_POST["send"] ){
-    // 送信ボタンを押下
-    $message = "お問い合わせを受け付けました\r\n"
-             . "名前" . $_SESSION["fullname"] . "\r\n"
-             . "email" . $_SESSION["email"] . "\r\n"
-             . "お問い合わせ内容:\r\n"
-             . preg_replace("/\r\n|\r|\n/", "\r\n", $_SESSION["message"] );
-    mail($_SESSION["email"], "お問い合わせありがとうございます", $message );
-    mail("hoge@fuga.com", "お問い合わせありがとうございます", $message );
+  } else if( isset($_POST['confirm']) && $_POST['confirm'] ){
+      // 確認画面
+    if( !$_POST['fullname'] ) {
+        $errmessage[] = "名前を入力してください";
+    } else if( mb_strlen($_POST['fullname']) > 100 ){
+        $errmessage[] = "名前は100文字以内にしてください";
+    }
+      $_SESSION['fullname'] = htmlspecialchars($_POST['fullname'], ENT_QUOTES);
+
+      if( !$_POST['email'] ) {
+          $errmessage[] = "Eメールを入力してください";
+      } else if( mb_strlen($_POST['email']) > 200 ){
+          $errmessage[] = "Eメールは200文字以内にしてください";
+    } else if( !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+        $errmessage[] = "メールアドレスが不正です";
+      }
+      $_SESSION['email']    = htmlspecialchars($_POST['email'], ENT_QUOTES);
+
+      if( !$_POST['message'] ){
+          $errmessage[] = "お問い合わせ内容を入力してください";
+      } else if( mb_strlen($_POST['message']) > 500 ){
+          $errmessage[] = "お問い合わせ内容は500文字以内にしてください";
+      }
+      $_SESSION['message'] = htmlspecialchars($_POST['message'], ENT_QUOTES);
+
+      if( $errmessage ){
+        $mode = 'input';
+    } else {
+        $mode = 'confirm';
+    }
+  } else if( isset($_POST['send']) && $_POST['send'] ){
+    // 送信ボタンを押したとき
+    $message  = "お問い合わせを受け付けました \r\n"
+              . "名前: " . $_SESSION['fullname'] . "\r\n"
+              . "email: " . $_SESSION['email'] . "\r\n"
+              . "お問い合わせ内容:\r\n"
+              . preg_replace("/\r\n|\r|\n/", "\r\n", $_SESSION['message']);
+      mail($_SESSION['email'],'お問い合わせありがとうございます',$message);
+    mail('sayaka.kudo.0913@.com','お問い合わせありがとうございます',$message);
     $_SESSION = array();
-    $mode = "send";
+    $mode = 'send';
   } else {
-    $SESSION = array();
+    $_SESSION['fullname'] = "";
+    $_SESSION['email']    = "";
+    $_SESSION['message']  = "";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -98,26 +127,38 @@
                 <a id="contactform"></a>
                 <h2 class="event-list">問い合わせフォーム</h2>
 
-                <?php if( $mode == "input" ){ ?>
+                <?php if( $mode == 'input' ){ ?>
     <!-- 入力画面 -->
-    <form action="./contactform.php" method="post">
-      名前 <input type="text" name="fullname" value="<?php echo $_SESSION["fullname"] ?>">
-      Eメール <input type="email" name="email" value="<?php echo $_SESSION["email"] ?>">
-      本文 <textarea name="message" id="" cols="" rows="" value="<?php echo $_SESSION["message"] ?>"></textarea>
-      <input type="submit" name="confirm" value="確認" class="button">
+    <?php
+      if( $errmessage ){
+        echo '<div style="color:red;">';
+        echo implode('<br>', $errmessage );
+        echo '</div>';
+      }
+    ?>
+
+    <form action="./contact.php" method="post">
+      名前    <br><input type="text"    name="fullname" value="<?php echo $_SESSION['fullname'] ?>"><br>
+      Eメール <br><input type="email"   name="email"    value="<?php echo $_SESSION['email'] ?>"><br>
+      お問い合わせ内容<br>
+      <textarea cols="70" rows="8" name="message"><?php echo $_SESSION['message'] ?></textarea><br>
+      <input class="check-botton" type="submit" name="confirm" value="確認" />
     </form>
-  <?php } else if( $mode == "confirm" ){ ?>
+
+  <?php } else if( $mode == 'confirm' ){ ?>
     <!-- 確認画面 -->
-    <form action="./cotactform.php" method="post">
-      名前 <?php echo $_SESSION["fullname"] ?>
-      Eメール <?php echo $_SESSION["email"] ?>
-      本文 <?php echo nl2br($_SESSION["message"]) ?>
+    <form action="./contact.php" method="post">
+      名前    <?php echo $_SESSION['fullname'] ?><br>
+      Eメール <?php echo $_SESSION['email'] ?><br>
+      お問い合わせ内容<br>
+      <?php echo nl2br($_SESSION['message']) ?><br>
       <input type="submit" name="back" value="戻る" />
       <input type="submit" name="send" value="送信" />
     </form>
+
   <?php } else { ?>
     <!-- 完了画面 -->
-    送信しました。お問い合わせありがとうございました。
+    送信しました。お問い合わせありがとうございました。<br>
   <?php } ?>
 
                 <a id="snsshare"></a>
